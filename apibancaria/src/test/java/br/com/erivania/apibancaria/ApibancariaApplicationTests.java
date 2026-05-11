e6e252d04fc57cbe5901504d75f2d51fd5841c6f
Erivania Leal <erivanial.s1507@gmail.com>
2026-05-11 20:25:24 -0300

    feat: criação do projeto e configuração do banco

---
+++ apibancaria/src/test/java/br/com/erivania/apibancaria/ApibancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erivania.apibancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
